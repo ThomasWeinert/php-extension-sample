905b7cb6a7f2f13ffcb51a169ae12e4a7970dff2
Thomas Weinert <thomas@weinert.info>
2016-09-11 13:41:54 +0200

    Hello World function in namespace

--- sample.c
+++ sample.c
@@ -1,9 +1,36 @@
 #include "php_sample.h"
 
+/*
+  1. define the namespace for your extension
+ */
+#define SAMPLE_NS "sample"
+
+/*
+  2. implement a php function
+ */
+PHP_FUNCTION(sample_hello_world)
+{
+  php_printf("Hello World!\n");
+}
+
+/*
+  3. define a list for your extension functions
+*/
+const zend_function_entry php_sample_functions[] = {
+  /*
+    4. register the function in the namespace
+   */
+  ZEND_NS_NAMED_FE(SAMPLE_NS, helloWorld, ZEND_FN(sample_hello_world), NULL)
+  PHP_FE_END
+};
+
 zend_module_entry sample_module_entry = {
   STANDARD_MODULE_HEADER,
   PHP_SAMPLE_EXTNAME,
-  NULL, /* Functions */
+  /*
+   5. add the functions to the module entry
+   */
+  php_sample_functions, /* Functions */
   NULL, /* MINIT */
   NULL, /* MSHUTDOWN */
   NULL, /* RINIT */
--- tests/001.phpt
+++ tests/001.phpt
@@ -4,7 +4,7 @@ Check for sample presence
 <?php if (!extension_loaded("sample")) print "skip"; ?>
 --FILE--
 <?php 
-echo "sample extension is available";
+echo \sample\helloWorld();
 ?>
 --EXPECT--
-sample extension is available
+Hello World!
