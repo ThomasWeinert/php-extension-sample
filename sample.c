82f8ab3898d7c7181445bf1cb5341f5a03805eac
Thomas Weinert <thomas@weinert.info>
2017-03-12 13:49:39 +0100

    Cleanup, add editor config

--- sample.c
+++ sample.c
@@ -1,37 +1,35 @@
 #include "php_sample.h"
 
-#define SAMPLE_NS "sample"
-
 PHP_FUNCTION(sample_getGreetingParts)
 {
-    /*
-     1. initialize return_value as array
-    */
-    array_init(return_value);
+	/*
+	 1. initialize return_value as array
+	*/
+	array_init(return_value);
 
-    /*
-     2. add elements with named key
-    */
-    add_assoc_str(return_value, "greeting", zend_string_init("Hello %s!", strlen("Hello %s!"), 0));
-    add_assoc_str(return_value, "who", zend_string_init("World", strlen("World"), 0));
+	/*
+	 2. add elements with named key
+	*/
+	add_assoc_str(return_value, "greeting", zend_string_init("Hello %s!", strlen("Hello %s!"), 0));
+	add_assoc_str(return_value, "who", zend_string_init("World", strlen("World"), 0));
 }
 
 const zend_function_entry php_sample_functions[] = {
-    ZEND_NS_NAMED_FE(SAMPLE_NS, getGreetingParts, ZEND_FN(sample_getGreetingParts), NULL)
-    PHP_FE_END
+	ZEND_NS_NAMED_FE(PHP_SAMPLE_EXT_NS, getGreetingParts, ZEND_FN(sample_getGreetingParts), NULL)
+	PHP_FE_END
 };
 
 zend_module_entry sample_module_entry = {
-    STANDARD_MODULE_HEADER,
-    PHP_SAMPLE_EXTNAME,
-    php_sample_functions, /* Functions */
-    NULL, /* MINIT */
-    NULL, /* MSHUTDOWN */
-    NULL, /* RINIT */
-    NULL, /* RSHUTDOWN */
-    NULL, /* MINFO */
-    PHP_SAMPLE_EXTVER,
-    STANDARD_MODULE_PROPERTIES
+	STANDARD_MODULE_HEADER,
+	PHP_SAMPLE_EXT_NAME,
+	php_sample_functions, /* Functions */
+	NULL, /* MINIT */
+	NULL, /* MSHUTDOWN */
+	NULL, /* RINIT */
+	NULL, /* RSHUTDOWN */
+	NULL, /* MINFO */
+	PHP_SAMPLE_EXT_VERSION,
+	STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_SAMPLE
