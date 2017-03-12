c9bf697d19cc7235c01827963503fd51d1a51c26
Thomas Weinert <thomas@weinert.info>
2017-03-12 13:51:59 +0100

    Cleanup, add editor config

--- sample.c
+++ sample.c
@@ -1,49 +1,47 @@
 #include "php_sample.h"
 
-#define SAMPLE_NS "sample"
-
 PHP_FUNCTION(sample_getGreetings)
 {
-    HashTable *names;
-    zval *entry;
+	HashTable *names;
+	zval *entry;
 
-    ZEND_PARSE_PARAMETERS_START(1, 1)
-    	Z_PARAM_ARRAY_HT(names)
-    ZEND_PARSE_PARAMETERS_END();
+	ZEND_PARSE_PARAMETERS_START(1, 1)
+		Z_PARAM_ARRAY_HT(names)
+	ZEND_PARSE_PARAMETERS_END();
 
-    /*
-      1. initialize the return value as an array
-    */
-    array_init(return_value);
+	/*
+	 1. initialize the return value as an array
+	*/
+	array_init(return_value);
 
-    ZEND_HASH_FOREACH_VAL(names, entry) {
-        zend_string *name = zval_get_string(entry);
-        zend_string *greeting = strpprintf(0, "Hello %s!", ZSTR_VAL(name));
-        zend_string_release(name);
+	ZEND_HASH_FOREACH_VAL(names, entry) {
+		zend_string *name = zval_get_string(entry);
+		zend_string *greeting = strpprintf(0, "Hello %s!", ZSTR_VAL(name));
+		zend_string_release(name);
 
-        /*
-          2. add greeting to list
-        */
-        add_next_index_str(return_value, greeting);
-    } ZEND_HASH_FOREACH_END();
+		/*
+		 2. add greeting to list
+		 */
+		add_next_index_str(return_value, greeting);
+	} ZEND_HASH_FOREACH_END();
 }
 
 const zend_function_entry php_sample_functions[] = {
-  ZEND_NS_NAMED_FE(SAMPLE_NS, getGreetings, ZEND_FN(sample_getGreetings), NULL)
-  PHP_FE_END
+	ZEND_NS_NAMED_FE(PHP_SAMPLE_EXT_NS, getGreetings, ZEND_FN(sample_getGreetings), NULL)
+	PHP_FE_END
 };
 
 zend_module_entry sample_module_entry = {
-  STANDARD_MODULE_HEADER,
-  PHP_SAMPLE_EXTNAME,
-  php_sample_functions, /* Functions */
-  NULL, /* MINIT */
-  NULL, /* MSHUTDOWN */
-  NULL, /* RINIT */
-  NULL, /* RSHUTDOWN */
-  NULL, /* MINFO */
-  PHP_SAMPLE_EXTVER,
-  STANDARD_MODULE_PROPERTIES
+	STANDARD_MODULE_HEADER,
+	PHP_SAMPLE_EXT_NAME,
+	php_sample_functions, /* Functions */
+	NULL, /* MINIT */
+	NULL, /* MSHUTDOWN */
+	NULL, /* RINIT */
+	NULL, /* RSHUTDOWN */
+	NULL, /* MINFO */
+	PHP_SAMPLE_EXT_VERSION,
+	STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_SAMPLE
