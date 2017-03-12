a1a920420348d1cd31d04a28918cf13049fe580b
Thomas Weinert <thomas@weinert.info>
2017-03-12 13:55:42 +0100

    Cleanup, add editor config

--- sample.c
+++ sample.c
@@ -1,16 +1,16 @@
 #include "php_sample.h"
 
 zend_module_entry sample_module_entry = {
-  STANDARD_MODULE_HEADER,
-  PHP_SAMPLE_EXTNAME,
-  NULL, /* Functions */
-  NULL, /* MINIT */
-  NULL, /* MSHUTDOWN */
-  NULL, /* RINIT */
-  NULL, /* RSHUTDOWN */
-  NULL, /* MINFO */
-  PHP_SAMPLE_EXTVER,
-  STANDARD_MODULE_PROPERTIES
+	STANDARD_MODULE_HEADER,
+	PHP_SAMPLE_EXT_NAME,
+	NULL, /* Functions */
+	NULL, /* MINIT */
+	NULL, /* MSHUTDOWN */
+	NULL, /* RINIT */
+	NULL, /* RSHUTDOWN */
+	NULL, /* MINFO */
+	PHP_SAMPLE_EXT_VERSION,
+	STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_SAMPLE
