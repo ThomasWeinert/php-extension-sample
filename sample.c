2e98e99d248a7c77e7312b95d8d4e3c936b61aff
Thomas Weinert <thomas@weinert.info>
2017-03-12 13:42:31 +0100

    Cleanup, add editor config

--- sample.c
+++ sample.c
@@ -1,44 +1,42 @@
 #include "php_sample.h"
 
-#define SAMPLE_NS "sample"
-
 PHP_FUNCTION(sample_multiply)
 {
-  /*
-   1. define/initialize variables
-   */
-  zend_long first;
-  zend_long second = 1;
+	/*
+	 1. define/initialize variables
+	 */
+	zend_long first;
+	zend_long second = 1;
 
-  /*
-    2. parse parameters using FAST ZPP Api
-    https://wiki.php.net/rfc/fast_zpp
-   */
-  ZEND_PARSE_PARAMETERS_START(1, 2)
-  	Z_PARAM_LONG(first)
-  	Z_PARAM_OPTIONAL
-  	Z_PARAM_LONG(second)
-  ZEND_PARSE_PARAMETERS_END();
+	/*
+	 2. parse parameters using FAST ZPP Api
+	 https://wiki.php.net/rfc/fast_zpp
+	 */
+	ZEND_PARSE_PARAMETERS_START(1, 2)
+		Z_PARAM_LONG(first)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_LONG(second)
+	ZEND_PARSE_PARAMETERS_END();
 
-  RETURN_LONG(first * second);
+	RETURN_LONG(first * second);
 }
 
 const zend_function_entry php_sample_functions[] = {
-  ZEND_NS_NAMED_FE(SAMPLE_NS, multiply, ZEND_FN(sample_multiply), NULL)
-  PHP_FE_END
+	ZEND_NS_NAMED_FE(PHP_SAMPLE_EXT_NS, multiply, ZEND_FN(sample_multiply), NULL)
+	PHP_FE_END
 };
 
 zend_module_entry sample_module_entry = {
-  STANDARD_MODULE_HEADER,
-  PHP_SAMPLE_EXTNAME,
-  php_sample_functions, /* Functions */
-  NULL, /* MINIT */
-  NULL, /* MSHUTDOWN */
-  NULL, /* RINIT */
-  NULL, /* RSHUTDOWN */
-  NULL, /* MINFO */
-  PHP_SAMPLE_EXTVER,
-  STANDARD_MODULE_PROPERTIES
+	STANDARD_MODULE_HEADER,
+	PHP_SAMPLE_EXT_NAME,
+	php_sample_functions, /* Functions */
+	NULL, /* MINIT */
+	NULL, /* MSHUTDOWN */
+	NULL, /* RINIT */
+	NULL, /* RSHUTDOWN */
+	NULL, /* MINFO */
+	PHP_SAMPLE_EXT_VERSION,
+	STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_SAMPLE
