2a0420aab51d7089ec0b1d1ddf9d4d8952f9f216
Thomas Weinert <thomas@weinert.info>
2017-03-12 11:43:13 +0100

    reformat to tab indent

--- sample.c
+++ sample.c
@@ -2,47 +2,47 @@
 
 PHP_FUNCTION(sample_hello_names)
 {
-  /*
-    1. define variables for the array
-  */
-  zval *args;
-  int argc, i;
+	/*
+	1. define variables for the array
+	*/
+	zval *args;
+	int argc, i;
 
-  /*
-    2. parse parameters using FAST ZPP Api
-    https://wiki.php.net/rfc/fast_zpp
-   */
-  ZEND_PARSE_PARAMETERS_START(1, -1)
-  	Z_PARAM_VARIADIC('+', args, argc)
-  ZEND_PARSE_PARAMETERS_END();
+	/*
+	2. parse parameters using FAST ZPP Api
+		https://wiki.php.net/rfc/fast_zpp
+	*/
+	ZEND_PARSE_PARAMETERS_START(1, -1)
+		Z_PARAM_VARIADIC('+', args, argc)
+	ZEND_PARSE_PARAMETERS_END();
 
-  /*
-    3. Iterate the arguments
-   */
-  for (i = 0; i < argc; i++) {
-  		zval *arg = args + i;
+	/*
+	3. Iterate the arguments
+	*/
+	for (i = 0; i < argc; i++) {
+		zval *arg = args + i;
 
-      convert_to_string(arg);
-      php_printf("Hello %s!\n", Z_STRVAL_P(arg));
-  }
+		convert_to_string(arg);
+		php_printf("Hello %s!\n", Z_STRVAL_P(arg));
+	}
 }
 
 const zend_function_entry php_sample_functions[] = {
-  ZEND_NS_NAMED_FE(SAMPLE_NS, hello, ZEND_FN(sample_hello_names), NULL)
-  PHP_FE_END
+	ZEND_NS_NAMED_FE(SAMPLE_NS, hello, ZEND_FN(sample_hello_names), NULL)
+	PHP_FE_END
 };
 
 zend_module_entry sample_module_entry = {
-  STANDARD_MODULE_HEADER,
-  PHP_SAMPLE_EXTNAME,
-  php_sample_functions, /* Functions */
-  NULL, /* MINIT */
-  NULL, /* MSHUTDOWN */
-  NULL, /* RINIT */
-  NULL, /* RSHUTDOWN */
-  NULL, /* MINFO */
-  PHP_SAMPLE_EXTVER,
-  STANDARD_MODULE_PROPERTIES
+	STANDARD_MODULE_HEADER,
+	PHP_SAMPLE_EXTNAME,
+	php_sample_functions, /* Functions */
+	NULL, /* MINIT */
+	NULL, /* MSHUTDOWN */
+	NULL, /* RINIT */
+	NULL, /* RSHUTDOWN */
+	NULL, /* MINFO */
+	PHP_SAMPLE_EXTVER,
+	STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_SAMPLE
