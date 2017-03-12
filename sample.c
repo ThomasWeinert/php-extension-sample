e5c829ebd745c772f22f83ac7f3514fdc2bdc988
Thomas Weinert <thomas@weinert.info>
2017-03-12 12:31:44 +0100

    Cleanup, add editor config

--- sample.c
+++ sample.c
@@ -1,48 +1,47 @@
 #include "php_sample.h"
 
-#define SAMPLE_NS "sample"
-
 /*
-  1. a name for the class
-*/
+ 1. a name for the class
+ */
 #define PHP_SAMPLE_CLASS_NAME "SampleClass"
 
 /*
-  2. a variable for the entry in the class list
-*/
+ 2. a variable for the entry in the class list
+ */
 static zend_class_entry *php_sample_class_entry;
 
 /*
-  3. a function list for the methods
-*/
+ 3. a function list for the methods
+ */
 const zend_function_entry php_sample_class_functions[] = {
-  PHP_ABSTRACT_ME(sample_Class, sampleMethod, NULL)
-  PHP_FE_END
+	PHP_ABSTRACT_ME(sample_Class, sampleMethod, NULL)
+	PHP_FE_END
 };
 
 PHP_MINIT_FUNCTION(sample)
 {
-    /*
-      4. Initialize and register the class
-    */
-    zend_class_entry ce;
-    INIT_NS_CLASS_ENTRY(
-      ce, SAMPLE_NS, PHP_SAMPLE_CLASS_NAME, php_sample_class_functions
-    );
-    php_sample_class_entry = zend_register_internal_class(&ce TSRMLS_CC);
+	/*
+	 4. Initialize and register the class
+	 */
+	zend_class_entry ce;
+
+	INIT_NS_CLASS_ENTRY(
+		ce, PHP_SAMPLE_EXT_NS, PHP_SAMPLE_CLASS_NAME, php_sample_class_functions
+	);
+	php_sample_class_entry = zend_register_internal_class(&ce TSRMLS_CC);
 }
 
 zend_module_entry sample_module_entry = {
-  STANDARD_MODULE_HEADER,
-  PHP_SAMPLE_EXTNAME,
-  NULL, /* Functions */
-  PHP_MINIT(sample), /* MINIT */
-  NULL, /* MSHUTDOWN */
-  NULL, /* RINIT */
-  NULL, /* RSHUTDOWN */
-  NULL, /* MINFO */
-  PHP_SAMPLE_EXTVER,
-  STANDARD_MODULE_PROPERTIES
+	STANDARD_MODULE_HEADER,
+	PHP_SAMPLE_EXT_NAME,
+	NULL, /* Functions */
+	PHP_MINIT(sample), /* MINIT */
+	NULL, /* MSHUTDOWN */
+	NULL, /* RINIT */
+	NULL, /* RSHUTDOWN */
+	NULL, /* MINFO */
+	PHP_SAMPLE_EXT_VERSION,
+	STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_SAMPLE
