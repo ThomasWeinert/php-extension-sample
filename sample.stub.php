0ab578aacb7681d07db73ba37bf40f875901d06b
Thomas Weinert <thomas@weinert.info>
2023-10-01 13:00:33 +0200

    Use Generated ArgInfo

---
+++ sample.stub.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sample {
+
+  class Greeting {
+
+    public function hello(): void {
+    }
+
+  }
+  
+}
---
+++ sample_arginfo.h
@@ -0,0 +1,5 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: b99c0317127354e774eba598cd9ff4c26f70c74e */
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Sample_Greeting_hello, 0, 0, IS_VOID, 0)
+ZEND_END_ARG_INFO()
